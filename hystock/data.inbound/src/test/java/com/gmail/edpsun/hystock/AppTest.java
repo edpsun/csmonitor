2231a993fbae964d646230adf0e11e3439ae4e20
sunp <edpsun@gmail.com>
2013-05-18 23:04:29 +0800

    derby test is ok

--- hystock/data.inbound/src/test/java/com/gmail/edpsun/hystock/AppTest.java
+++ hystock/data.inbound/src/test/java/com/gmail/edpsun/hystock/AppTest.java
@@ -1,5 +1,9 @@
 package com.gmail.edpsun.hystock;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.Properties;
 
 import org.junit.Test;
@@ -22,6 +26,23 @@ public class AppTest extends AbstractJUnit4SpringContextTests {
         p.load(this.getClass().getClassLoader()
                 .getSystemResourceAsStream("properties/conf.properties"));
         System.out.println(p.get("scope"));
-        applicationContext.getBean("greetMeBean",GreetMeBean.class).execute();
+        applicationContext.getBean("greetMeBean", GreetMeBean.class).execute();
+    }
+
+    @Test
+    public void testDerby() throws Exception {
+        Class.forName("org.apache.derby.jdbc.ClientDriver").newInstance();
+        String nsURL = "jdbc:derby://localhost:1527/hystock";
+        java.util.Properties props = new java.util.Properties();
+        props.put("user", "admin");
+        props.put("password", "admin123");
+        Connection conn = DriverManager.getConnection(nsURL, props);
+        Statement s = conn.createStatement();
+
+        ResultSet rs = s.executeQuery("SELECT * FROM MEMREPORT");
+        while (rs.next()) {
+            System.out.println(rs.getString(1) + " : " + rs.getString(2));
+        }
+
     }
 }
