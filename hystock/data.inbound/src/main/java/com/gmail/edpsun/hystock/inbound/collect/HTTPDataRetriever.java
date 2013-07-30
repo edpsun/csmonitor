3c0f81f3210ebe1f62004d16cabd780db9e87c99
sunp <edpsun@gmail.com>
2013-07-30 16:22:38 +0800

    fix 403 error

--- hystock/data.inbound/src/main/java/com/gmail/edpsun/hystock/inbound/collect/HTTPDataRetriever.java
+++ hystock/data.inbound/src/main/java/com/gmail/edpsun/hystock/inbound/collect/HTTPDataRetriever.java
@@ -22,6 +22,8 @@ public class HTTPDataRetriever implements DataRetriever {
         try {
             URL _url = new URL(url);
             httpConnection = (HttpURLConnection) _url.openConnection();
+            httpConnection.setRequestProperty("User-Agent", "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.4; en-US; rv:1.9.2.2) Gecko/20100316 Firefox/3.6.2");
+
             InputStream in = httpConnection.getInputStream();
             BufferedReader br = new BufferedReader(new InputStreamReader(in, "GB2312"));
 
